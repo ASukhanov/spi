c86cd65db7234e033391e66a3936e3ef7cb087d7
ASukhanov <sukhanov@bnl.gov>
2015-07-13 15:10:56 -0400

    ad5592.sh

--- ad5592.sh
+++ ad5592.sh
@@ -4,7 +4,7 @@ usage ()
 cat << EOF
 usage: usage: $0 options
 
-Control of the AD5592
+Control of the AD5592 ADC/DAC
 
 OPTIONS:
   options
@@ -12,22 +12,36 @@ OPTIONS:
   -r N  Read N 16-bit words
   -a M  read ADC sequence of channels specified by hex mask M, may follow with -r N
   -t M  same as -a M but includes temperature in the sequence
+  -x    execute
 
 EXAMPLE:
-  $0
+  read ADC[4] and  ADC[0]:
+  ./ad5592.sh -v -a16#11 -r3	# first 4 bytes should be ignored
 EOF
 }
-#c=''; for i in `seq 0 9`; do c=$c$i; done; echo $c
+((Reg_Sequencer_REP=16#200))	#repeat
+((Reg_Sequencer_TEMP=0))	#temperature, if needed set to 16#100
+BYTES=""
+function read_adc {
+  ((Reg_Sequencer=(16#2)<<11 | $1 | $Reg_Sequencer_REP | $Reg_Sequencer_TEMP))
+  printf '%04x\n' $Reg_Sequencer
+  printf 'Reg_Sequence = %04x\n' $Reg_Sequencer
+  HEX=`printf '%04x\n' $Reg_Sequencer`
+  BYTES=$BYTES\\x${HEX:2:2}\\x${HEX:0:2}
+}
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 OPTIND=1    	# do not skip arguments
-while getopts "vr:a:t:h" opt; do
+while getopts "vr:a:t:hx" opt; do
   #echo "opt=$opt"
   case $opt in
-	v)  VERB="-v";;
-	r)  for i in `seq 1 $OPTARG`; do c=$c'0'; done; echo $c; eval ./spidev_test -p $c $VERB;;
-	h)  usage;;
-	?)  echo "ERROR, Illegal option"; exit 1;;
-	*)  ;;
+    v) VERB="-v";;
+    r) for i in `seq 1 $OPTARG`; do BYTES=$BYTES'0'; done;;
+    a) read_adc $OPTARG;;
+    t) ((Reg_Sequencer_TEMP=16#100)); read_adc $OPTARG;;
+    x) CMD="./spidev_test $VERB -p\"$BYTES\""; echo $CMD; eval $CMD;;
+    h) usage;;
+    ?) echo "ERROR, Illegal option"; exit 1;;
+    *)  ;;
   esac
 done
 
