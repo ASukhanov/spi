299df400570c6db552ce2ede5cb0859441b6d348
ASukhanov <sukhanov@bnl.gov>
2015-07-15 14:54:55 -0400

    ad5592.sh added -H for ad5592 to work on negative front of the clock init corrected.

--- ad5592.sh
+++ ad5592.sh
@@ -10,39 +10,48 @@ OPTIONS:
   options
   -v    Verbosity
   -i    Init
-  -a M  Schedule the reading of ADC sequence of channels specified by hex mask M, may follow with $
-  -t M  Same as -a M but includes temperature in the sequence
-  -r R  Select register for future write operation
-  -w W  Schedule the writing of 11 bits W[10:0] into the selected register
-  -x N  Execute the scheduled transfers, followed by reading of N 16-bit words
-  -b R  Read back the setting of register R
-  -z    reset
+  -a M  Schedule the reading of ADC channels specified by hex mask M.
+  -t M  Same as -a M but includes temperature in the sequence.
+  -r R  Select register for future write operation.
+  -w W  Schedule the writing of 11 bits W[10:0] into the selected register.
+  -x N  Execute the scheduled transfers, followed by the reading of N 16-bit
+        words.
+  -b R  Read back the setting of register R.
+  -z    reset.
 
 EXAMPLES:
   reset and read Reg[6]
   $0 -z -b6
-  should give you:
+  the result should be:
 RX | 00 00 __ __ __ __ __ __ __ __ __ __ __ __ __ __  | ..
 RX | 00 00 00 7F __ __ __ __ __ __ __ __ __ __ __ __  | ..
 
+  write to General Purpose Register 0x00F0 and read back
+  $0 -v -r3 -w16#f0 -x1 -b3
+  the result should be: RX | 00 00 00 F0
+
+  read back all registers:
+  $0 -b1 -b2 -b3 -b4 -b5 -b6 -b7 -b8 -b9 -b10 -b11 -b12 -b13
+
   write 0x1234 into reg[3] and read back its content
   $0 -v -r3 -w16#1234 -x1 -b3
+
   read ADC[4] and  ADC[0]:
   $0 -v -a16#11 -r3     # first 4 bytes should be ignored
 EOF
 }
 #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-#SPIDEV_CMD="./ad5592 -d0 -l"
-SPIDEV_CMD="./ad5592 "
+SPIDEV_CMD="./ad5592 -H"	#ad5592 reacts on falling edge of the clock
 VERB=""
                                  #5432109876543210
-((Reg_General_Purpose_Control = 2#0001100000110000)) #
-((Reg_ADC_Config              = 2#0010100011110000)) #
-((Reg_DAC_Config              = 2#0001100000001111)) #
+((Reg_General_Purpose_Control = 2#0001101100110000)) #
+# addr=3, ADC buffer enabled, Lock off, not AllDACs, ADC range 2x, DAC: 2x.
+((Reg_ADC_Config              = 2#0010000111110000)) #
+((Reg_DAC_Config              = 2#0010100000001111)) #
 ((Reg_Readback                = 2#0011100001000000)) #
 ((Reg_Reset                   = 2#0111110110101100)) #
-((Reg_Sequencer_REP=16#200))    #repeat
+((Reg_Sequencer_REP           = 2#0001001000000000))    #repeat
 ((Reg_Sequencer_TEMP=0))        #temperature, if needed set to 16#100
 twoz="\x00\x00"
 #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
