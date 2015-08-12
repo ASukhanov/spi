7a243c3cca77242b4c9a8e6864b4bc1310daa157
ASukhanov <sukhanov@bnl.gov>
2015-08-12 16:58:56 -0400

    deleted:    dadc_server_start.sh

--- dadc_server_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start dadc monitor on the server
-ssh pi@130.199.23.244 "spi/dadc_sshout.sh"
-
--- dadc_watch_adc.sh
+++ dadc_watch_adc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This is obsolete, 'dadcmon -m'  will do the job 
 # Watch 7 adc channels and temperature
 #./ad5592.sh -a0x17f -x0
 ./ad5592.sh -a0x37f -x0
