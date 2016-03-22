968fd567ddfae05f83b6e162675499589d8938f0
ASukhanov <sukhanov@bnl.gov>
2016-03-22 17:27:55 -0400

    AD5592 channel maps

---
+++ ad5592-v6_map.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Channel map of EMCO_AD5592-v6 board
+#
+export AD5592_E_MON=0
+export AD5592_E_PRG=1
+export AD5592_BIAS_CM=2
+export AD5592_AV_ADJ=3
+export AD5592_AVDD=4
+export AD5592_DV_ADJ=5
+export AD5592_DVDD=6
+
---
+++ ad5592-v7_map.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Channel map of EMCO_AD5592-v7 board
+#
+export AD5592_E_MON=0
+export AD5592_BIAS_CN=1
+export AD5592_E_PRG=2
+export AD5592_HIRANGE=3
+export AD5592_DVDD=4
+export AD5592_AVDD=5
+export AD5592_DC_A0=6
+export AD5592_DC_A1=7
