845fa6dbd909e43183b5eb8505b82c70281b77bc
Andrey <andrey@phast.bnl.gov>
2016-04-09 18:32:01 -0400

    chart_ad5592.py

--- chart_ad5592.py
+++ chart_ad5592.py
@@ -4,13 +4,15 @@ fn='/tmp/dadc.log'
 #version 1 2015-07-21
 #version 2 2015-08-17  pastdata feature added
 
+# Parameters
+#MAXY=4100
+MAXY=3500
+NP=1000
+NG=9
+
 import pylab as pl
 import time
 
-#MAXY=4100
-MAXY=1000
-NP=1000
-NG=9
 X = pl.arange(0,NP,1)
 sp = []
 Y = []
@@ -40,15 +42,17 @@ while True:
       pl.pause(1) # provides interactive chart control, but takes more CPU time
       #time.sleep(1) #this takes much less of CPU but no interactive control of the chart
       continue
+    #print (latest_data)
     ii += 1
-    fday,ftime,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8] = latest_data.split(' ')
+    fday,ftime,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8] = latest_data[:64].split(' ')
     
+    '''
     #scale some channels
     d[3] = str(4096 - int(d[3])) # voltage drop on D2 shottky
     d[5] = str(2500 - int(d[5])) # 2.5V
     d[7] = str(2500 - int(d[7])) # 2.5V
-    #print(d)
-
+    print(d)
+    '''
     for nn in range(NG):
       Y[nn][ii%NP] = d[nn]
       Y[nn][(ii+1)%NP] = 0
