43e6bb5ab22539c02fbe4c83eb6c1a6fc059de2c
ASukhanov <sukhanov@bnl.gov>
2016-04-09 11:40:46 -0400

    -

--- dadc_view.sh
+++ dadc_view.sh
@@ -4,7 +4,7 @@
 if [ -n "$EMCO_AD5592_v5" ];
 then
   TITLE="EMCO_AD5592_v5 Monitor"
-  FIELDS "date------ time---- BMon BPrg IBHR ad3  ad4  DVDD ad6  AVDD temp"
+  FIELDS="date------ time---- BMon BPrg IBHR ad3  ad4  DVDD ad6  AVDD temp"
 else
   TITLE="EMCO_AD5592_v7 Monitor"
   FIELDS="date------ time---- BMon IBHR BPrg IBLR ad4  ad5  AVDD DVDD temp"
