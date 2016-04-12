ffa0d15daf7e23fbe12ecf736fa36b5c1b5e2887
ASukhanov <sukhanov@bnl.gov>
2016-04-12 16:30:52 -0400

    -

--- dadc_get.sh
+++ dadc_get.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 # Get readings of 8 ADC channels and temperature from the server with the EMCO_AD5592 board.
 # The DADC_SERVER should be exported prior to running this script i.e.:
 # $ export DADC_SERVER=pi@130.199.23.244
