0057c7a83f217dc5abf218c1084ded64d23a30ff
ASukhanov <sukhanov@bnl.gov>
2016-04-13 10:39:45 -0400

    new file:   stripchart_log.py

--- stripchart_log.py
+++ stripchart_log.py
@@ -1,14 +1,16 @@
+#!/bin/bash
 '''
-9-line Stripchart using rollling array 
+Dynamic chart of a changing log file 
 '''
-import time
 
+fn='/tmp/dadc.log'
+NCurves = 9
+NPoints = 1000
+
+import time
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
-
-NCurves = 9
-NPoints = 1000
 
 #### PyQt GUI setup ###########################################################
 app = QtGui.QApplication([])
@@ -19,6 +21,12 @@ win.resize(NPoints,600)
 pg.setConfigOptions(antialias=True)
 
 randplot = win.addPlot(title="Data")
+
+''' TODO add colored legend
+for ii in range(NCurves):
+  label = pg.TextItem("test", anchor=(0.1+ii/10., 0.5))
+  label.setParentItem(randplot)
+'''
 
 grf = []
 for ii in range(NCurves):
@@ -31,15 +39,32 @@ for ii in range(NCurves):
 
 data = np.zeros((NCurves,NPoints),dtype=np.int)
 
+f = open(fn)
+p = 0
 step = 0
+pastdata = True
+d = [0 for ii in range(NCurves)]
+
 def update():
-    global step
+    global f,p,step,d
     step = (step + 1) % NPoints
+    f.seek(p)
+    latest_data = f.readline()
+    p = f.tell()
+    if len(latest_data)==0:
+      return
+    #print('['+latest_data+']')
+    try: 
+      fday,ftime,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8] = latest_data[:65].split(' ')
+    except:
+      print(latest_data),
+      return
     #
     # roll and update data arrays
     for ii in range(NCurves):
       data[ii][1:] = data[ii][:-1]
-      data[ii][:1] = rndm[ii][step] + ii*10
+      #data[ii][:1] = rndm[ii][step] + ii*10
+      data[ii][:1] = d[ii]
     #
     # update plots 
     for ii in range(NCurves):
