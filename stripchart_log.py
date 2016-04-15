871591e5cd3757d1a7f16b444f66ee955eb950b6
ASukhanov <sukhanov@bnl.gov>
2016-04-15 10:33:50 -0400

    stripchart_log.py: -t option

--- stripchart_log.py
+++ stripchart_log.py
@@ -1,7 +1,9 @@
 #!/bin/bash
-version = 'v1 2016-04-13'
+version = 'v2 2016-04-14 option -t'
 def usage():
-  print('Dynamic chart of a changing log file. '+version+', usage: '+sys.argv[0]+ ' [-b]')
+  print('Dynamic chart of a changing log file. '+version)
+  print(' usage: '+sys.argv[0]+ ' [-t N]')
+  print('   Option -t: start logging N hours after beginning (N>0) or before the end (N<0) of the file.')
 
 # Setting
 fn='/tmp/dadc.log'
@@ -9,6 +11,7 @@ NCurves = 9
 NPoints = 1000
 Tail = True
 labels = ('00:00:00','BMon','IBHR','BPrg','IBLR','ad4','ad5','AVDD','DVDD','Temp')
+Logline_length = 64
 
 import sys
 import getopt
@@ -18,14 +21,17 @@ import numpy as np
 import pyqtgraph as pg
 
 try:
-  opts,args = getopt.getopt(sys.argv[1:], 'hb', ["help", "beginning"])
+  opts,args = getopt.getopt(sys.argv[1:], 'ht:', ["help", "time"])
 except getopt.GetoptError as err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
+fpos = 0
 for o, a in opts:
-        if o == "-b":
+        if o == "-t":
+            fpos = int(3600*float(a)*Logline_length)
+            print('fpos='+str(fpos))
             Tail = False
         elif o in ("-h", "--help"):
             usage()
@@ -41,6 +47,7 @@ win.resize(NPoints,600)
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
 
+# Place labels
 label = []
 for ii in range(NCurves+1):
   label.append(pg.LabelItem(labels[ii],justify='right',bold=True,size='14pt'))
@@ -50,36 +57,49 @@ for ii in range(NCurves+1):
     tcolor = 'w'
   label[ii].setText(labels[ii],color=tcolor)
   win.addItem(label[ii],row=0,col=ii)
-  #win.addLabel(labels[ii],row=0,col=ii,color=pg.intColor(ii))
 
+# Add curves
 randplot = win.addPlot(row=1,col=0,colspan=NCurves+1)
-
 grf = []
 for ii in range(NCurves):
   grf.append(randplot.plot(pen=(ii,NCurves)))
 
 data = np.zeros((NCurves,NPoints),dtype=np.int)
-
-f = open(fn,'rU')
-step = 0
-pastdata = True
 d = [0 for ii in range(NCurves)]
+step = 0
 
+# Open file and set position
+f = open(fn,'rU')
+if Tail:
+  #f.seek(-(LogLine_length),2)
+  #f.readlines()[-1].decode()
+  fpos = f.tell()
+else:
+  whence = 0 if fpos>0 else 2
+  print
+  f.seek(fpos, whence)
+print('file '+fn+' opened and positioned to '+str(f.tell()))
+
+# Define update function
 def update():
-    global f,step,d,Tail
+    global f,step,d,Tail,fpos
     for  lines in range(NPoints):
       step = (step + 1) % NPoints
       if Tail:
-        f.seek(-(65+5),2)
-        latest_data = f.readlines()[-1].decode()
-      else:
-        latest_data = f.readline()
+        f.seek(fpos)
+      latest_data = f.readline()
+      if Tail:
+        fpos = f.tell()
       #print('l='+str(len(latest_data)))
-      if len(latest_data)<10: #too short
+      if len(latest_data)<Logline_length: #too short
         if len(latest_data)==0: # reached end of file
-          print('End of file reached, please restart program without -b option.')
-          Tail = True 
+          if not Tail:
+            print('End of file reached.')
+            Tail = True
+            fpos = f.tell()
+        time.sleep(1)
         return
+      #print(latest_data)
       try: 
         fday,ftime,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8] = latest_data.split(' ')
       except:
@@ -102,10 +122,7 @@ def update():
       grf[ii].setData(data[ii])
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-if Tail:
-  timer.start(1000)
-else:
-  timer.start(100)
+timer.start(50)
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
