4841f2599b70e825ee353add4c6c8652791180f3
ASukhanov <sukhanov@bnl.gov>
2016-04-15 16:46:53 -0400

    stripchart_log.py option  -m

--- stripchart_log.py
+++ stripchart_log.py
@@ -2,16 +2,20 @@
 version = 'v2 2016-04-14 option -t'
 def usage():
   print('Dynamic chart of a changing log file. '+version)
-  print(' usage: '+sys.argv[0]+ ' [-t N]')
+  print(' usage: '+sys.argv[0]+ ' [-t N] [-m]')
   print('   Option -t: start logging N hours after beginning (N>0) or before the end (N<0) of the file.')
+  print('   Option -m: Display means')
 
 # Setting
 fn='/tmp/dadc.log'
 NCurves = 9
 NPoints = 1000
+Logline_length = 64
 Tail = True
 labels = ('00:00:00','BMon','IBHR','BPrg','IBLR','ad4','ad5','AVDD','DVDD','Temp')
-Logline_length = 64
+Means = False
+MeanRange = 10
+means = ('Means:','?','?','?','?','?','?','?','?','?')
 
 import sys
 import getopt
@@ -21,7 +25,7 @@ import numpy as np
 import pyqtgraph as pg
 
 try:
-  opts,args = getopt.getopt(sys.argv[1:], 'ht:', ["help", "time"])
+  opts,args = getopt.getopt(sys.argv[1:], 'hmt:', ["help", "mean", "time"])
 except getopt.GetoptError as err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -31,13 +35,15 @@ fpos = 0
 for o, a in opts:
         if o == "-t":
             fpos = int(3600*float(a)*Logline_length)
-            print('fpos='+str(fpos))
+            #print('fpos='+str(fpos))
             Tail = False
+        elif o == "-m":
+            Means = True
         elif o in ("-h", "--help"):
             usage()
             sys.exit()
         else:
-            assert False, "unhandled option"
+            assert False, "unhandled option "
 
 #### PyQt GUI setup ###########################################################
 app = QtGui.QApplication([])
@@ -47,6 +53,7 @@ win.resize(NPoints,600)
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
 
+row = 0
 # Place labels
 label = []
 for ii in range(NCurves+1):
@@ -56,10 +63,19 @@ for ii in range(NCurves+1):
   else:
     tcolor = 'w'
   label[ii].setText(labels[ii],color=tcolor)
-  win.addItem(label[ii],row=0,col=ii)
+  win.addItem(label[ii],row=row,col=ii)
+
+# Place means
+if Means:
+  mean = []
+  row = row + 1
+  for ii in range(NCurves+1):
+    mean.append(pg.LabelItem(means[ii],justify='right',bold=True,size='14pt'))
+    win.addItem(mean[ii],row=row,col=ii)
 
 # Add curves
-randplot = win.addPlot(row=1,col=0,colspan=NCurves+1)
+row = row + 1
+randplot = win.addPlot(row,col=0,colspan=NCurves+1)
 grf = []
 for ii in range(NCurves):
   grf.append(randplot.plot(pen=(ii,NCurves)))
@@ -82,7 +98,7 @@ print('file '+fn+' opened and positioned to '+str(f.tell()))
 # Define update function
 def update():
     # get new data in data[]
-    global f,step,d,Tail,fpos
+    global f,step,d,Tail,fpos,mean
     for  lines in range(NPoints):
       step = (step + 1) % NPoints
       if Tail:
@@ -120,6 +136,9 @@ def update():
     # update plots with new data
     for ii in range(NCurves):
       grf[ii].setData(data[ii])
+    if Means:
+      for ii in range(NCurves):
+        mean[ii+1].setText(str(np.mean(data[ii][:MeanRange])))
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
