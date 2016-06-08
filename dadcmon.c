6a373333419571a041653b9df095cb4962ab77e4
PHENIX Run Control <phnxrc@ncc0.phy.bnl.gov>
2016-06-08 17:42:11 -0400

    dadcmon.c: violation count

--- dadcmon.c
+++ dadcmon.c
@@ -17,6 +17,7 @@
 // Version v5 20160411  print "RX only if verbose is set
 // Version v6 20160413  safety trips: IBLR, BPrg
 // TODO: add one more trip when difference BPrg-BMon is significant
+// Version v7 20160601  MAX_VIOLATIONS
 
 // Set the guard sense channel, trip channel and the trip level.
 /* For PCB v5
@@ -28,12 +29,14 @@ static int guard_trip_channel = 1; // trip channel, it will be set to 0
 static int guard_sense_channel = 0; // sense channel
 static int guard_trip_level = 4100; // trip level for the sense channel
 static int guard_trip_channel = 2; // trip channel, it will be set to 0
-static int guard_trip_level_IBLR = 40; // ~10uA Hi current trip
+//static int guard_trip_level_IBLR = 40; // ~10uA Hi current trip
+static int guard_trip_level_IBLR = 60;
 static int guard_channel_IBLR = 3;
 static int guard_channel_BPrg = 2;
 static int guard_trip_level_BPrg = 1420; // ~420V, not safe for capacitors
 /**/
-
+#define MAX_VIOLATIONS 4
+static int violation_count = 0;
 
 #include <stdint.h>
 #include <unistd.h>
@@ -197,6 +200,7 @@ static void decode_rx(uint8_t *rx, int nn, int *channels)
   //v3printf("\n");
   fflush(stdout);
 }
+
 static void guard(int adc_channel, int trip_level, int dac_channel)
 {
   uint8_t dacb[] = {0, 0};
@@ -206,24 +210,35 @@ static void guard(int adc_channel, int trip_level, int dac_channel)
   //safety trips
   if (channels[guard_channel_IBLR] > guard_trip_level_IBLR)
   {
-    printf("#Trip IBLR! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to 0\n",
-      guard_channel_IBLR, channels[guard_channel_IBLR], guard_trip_level_IBLR, guard_channel_BPrg);
-    *dacw = 0x8000 | (guard_channel_BPrg&0x7)<<12;
+    violation_count++;
+    if(violation_count>MAX_VIOLATIONS)
+    {
+      printf("#Trip IBLR! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to 0\n",
+        guard_channel_IBLR, channels[guard_channel_IBLR], guard_trip_level_IBLR, guard_channel_BPrg);
+      *dacw = 0x8000 | (guard_channel_BPrg&0x7)<<12;
+    }
   }
   else if (channels[guard_channel_BPrg] > guard_trip_level_BPrg)
   {
-    printf("#Trip BPrg! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to 0\n",
-      guard_channel_BPrg, channels[guard_channel_BPrg], guard_trip_level_BPrg, guard_channel_BPrg);
-    *dacw = 0x8000 | (guard_channel_BPrg&0x7)<<12;
-  }
+    violation_count++;
+    if(violation_count>MAX_VIOLATIONS)
+    {
+      printf("#Trip BPrg! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to 0\n",
+        guard_channel_BPrg, channels[guard_channel_BPrg], guard_trip_level_BPrg, guard_channel_BPrg);
+      *dacw = 0x8000 | (guard_channel_BPrg&0x7)<<12;
+  } }
   //user defined trip
   else if (channels[adc_channel] > trip_level)
   {
-    printf("#Trip! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to %04x\n",
-      adc_channel,channels[adc_channel],trip_level,dac_channel,trip_value);
-    *dacw = 0x8000 | (dac_channel&0x7)<<12 | (trip_value&0xfff);
-  }
-  else return;
+    violation_count++;
+    if(violation_count>MAX_VIOLATIONS)
+    {
+      printf("#Trip! ADC[%1i]=%i exceeds %i, DAC[%1i] is set to %04x\n",
+        adc_channel,channels[adc_channel],trip_level,dac_channel,trip_value);
+      *dacw = 0x8000 | (dac_channel&0x7)<<12 | (trip_value&0xfff);
+  } }
+  else
+  { violation_count = 0; return;}
 
   bb=dacb[0]; dacb[0]=dacb[1]; dacb[1]=bb; //swap bytes
   transfer(dacb, ir, 2);
